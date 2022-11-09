5f7b978f01cf560aa6728f06f1f29a58938c676f
Sol Oliveira <sol.oliveira@elevenfinancial.com>
2022-11-09 17:10:57 -0300

    add: banner da aplicação

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="main-container">
-    <h1>HOME</h1>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>Monte o seu burger:</h1>
+  
+    </div>
   </div>
 </template>
 
 <script>
+import Banner from '../components/Banner'
 
-
-
+export default {
+  name: 'Home',
+  components: {
+    Banner,
+    
+  }
+}
 </script>
